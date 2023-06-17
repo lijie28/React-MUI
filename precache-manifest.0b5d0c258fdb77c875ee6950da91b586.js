c75bee56dff4fe5adc23a267325894aa7e35912f
lijie <lijie9228@hotmail.com>
2023-06-18 00:03:45 +0900

    Updates

---
+++ precache-manifest.0b5d0c258fdb77c875ee6950da91b586.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3f1efae53b35c88fdbf",
+    "url": "/react-mui/static/css/main.03ef0d21.chunk.css"
+  },
+  {
+    "revision": "f3f1efae53b35c88fdbf",
+    "url": "/react-mui/static/js/main.f3f1efae.chunk.js"
+  },
+  {
+    "revision": "382abf35e3d879242896",
+    "url": "/react-mui/static/js/1.382abf35.chunk.js"
+  },
+  {
+    "revision": "0e1e15404f839d5f84b7",
+    "url": "/react-mui/static/js/2.0e1e1540.chunk.js"
+  },
+  {
+    "revision": "7f02f87f138390060aa2",
+    "url": "/react-mui/static/js/runtime~main.7f02f87f.js"
+  },
+  {
+    "revision": "55521548e5e22be48c073078bfc9dbc4",
+    "url": "/react-mui/static/media/testCat.55521548.png"
+  },
+  {
+    "revision": "d4028d8df47f8068fc47120e056145db",
+    "url": "/react-mui/static/media/testMan.d4028d8d.jpg"
+  },
+  {
+    "revision": "7741da13d20445a786e6a9d3b96a0da3",
+    "url": "/react-mui/static/media/testWoman.7741da13.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-mui/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b5c5ed1f5ca946b4f7a87ef5d0e48a04",
+    "url": "/react-mui/index.html"
+  }
+];
